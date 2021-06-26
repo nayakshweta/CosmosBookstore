5e30a7c87c139346fa47e4c3513cb865130af413
Shweta Nayak <16984456+nayakshweta@users.noreply.github.com>
2021-06-26 13:02:43 +0000

    Fixes image CSS for books list

--- src/Home/BooksListItem.js
+++ src/Home/BooksListItem.js
@@ -3,13 +3,10 @@ import { Link } from 'react-router-dom';
 
 export const BooksListItem = ({ book }) => (
     <li>
-        <figure>
-            <img className="img-book-cover" src={book.img} alt={book.title}></img>
-            <figcaption>
-                
-                <h3>{book.title}</h3>
-            </figcaption>
-        </figure>
-        <p>by {book.author}</p>
+        <a href={"/books/" + book._id }>
+            <figure>
+                <img src={book.img} alt={book.title}></img>
+            </figure>
+        </a>
     </li>
 );
