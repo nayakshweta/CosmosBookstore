44b0fa3ce19de4bdb7738b169d12ca2b33b09431
Shweta Nayak <16984456+nayakshweta@users.noreply.github.com>
2021-06-27 10:46:04 +0000

    Uses classes instead of ids for Navbar CSS.

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 export const NavBar = () =>  (
     <nav role="navigation">
-        <div id="menuToggle">
+        <div className="menuToggle">
             <input type="checkbox" />
             <span></span>
             <span></span>
             <span></span>
-            <ul id="menu">
-            <a href="#"><li>Home</li></a>
-            <a href="#"><li>About</li></a>
+            <ul className="menu">
+                <a href="#"><li>Home</li></a>
+                <a href="#"><li>About</li></a>
             </ul>
         </div>
     </nav>
