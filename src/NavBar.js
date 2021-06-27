25f71502c1b84a39c6ddbe65846fb88bc542a655
Shweta Nayak <16984456+nayakshweta@users.noreply.github.com>
2021-06-27 12:17:56 +0000

    Adds search box to top navbar.

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,17 +3,23 @@ import React from 'react';
 
 export const NavBar = () =>  (
     <header className="navbar">
-    <nav role="navigation">
-        <div className="menuToggle">
-            <input type="checkbox" />
-            <span></span>
-            <span></span>
-            <span></span>
-            <ul className="menu">
-                <a href="#"><li>Home</li></a>
-                <a href="#"><li>About</li></a>
-            </ul>
+        <nav role="navigation">
+            <div className="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul className="menu">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>About</li></a>
+                </ul>
+            </div>
+        </nav>
+        <div className="search-box">
+            <div className="field">
+                <input type="search" name="search" placeholder="Find your next book to read..." className="search-query"/>
+                <button type="submit" className="btn search-submit-btn">Search</button>
+            </div>
         </div>
-    </nav>
     </header>
 );
