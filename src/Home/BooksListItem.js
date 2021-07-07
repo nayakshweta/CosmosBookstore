a4d588ff383543a66612ff0576956f522f1c570f
Shweta Nayak <16984456+nayakshweta@users.noreply.github.com>
2021-07-07 13:35:03 +0000

    Adds default book background with title for books with missing images on the book list page.

--- src/Home/BooksListItem.js
+++ src/Home/BooksListItem.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const BooksListItem = ({ book }) => {
 
     const title = (book.title.length < 100 ) ? book.title : (book.title.substring(0, 100) + '...');
     const author = book.author.length < 100 ? book.author : book.author.substring(0,100) + '...';
 
+    let myStyle;
+    let titleTextImg;
+    let authorTextImg;
+    if (book.img != "") {
+        const imageUrl = book.img;
+        myStyle = {backgroundImage: 'url(' + imageUrl + ')', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '450px', borderRadius: '1rem'};
+        titleTextImg = "";
+        authorTextImg = "";
+    } else {
+        const imageUrl = "/bookcover.png";
+        myStyle = {backgroundImage: 'url(' + imageUrl + ')', 
+                    backgroundPosition: 'center', 
+                    backgroundSize: 'cover', 
+                    backgroundRepeat: 'no-repeat', 
+                    height: '450px', 
+                    borderRadius: '1rem',
+                    textAlign: "center",
+                    fontSize: "30px",
+                    color: "black",
+                    lineHeight: "80px",
+                    fontFamily: "Times New Roman",
+                    textTransform: "uppercase",
+                    };
+        titleTextImg = title;
+        authorTextImg = author;
+    }
+
     return (<li>
         <a href={"/books/" + book._id } target="_blank">
-        <div 
-            className="item" 
-            style={{ backgroundImage: 'url(' + book.img + ')', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '450px', borderRadius: '1rem'}}>
-        
+        <div className="item" style={myStyle}>{titleTextImg}
             <div className="item-overlay">
                 <div className="book-title">{title}</div>
                 <div className="book-author">By {author}</div>
