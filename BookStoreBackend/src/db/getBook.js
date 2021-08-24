be2f6f393c0abf8b6aab6a854fe9a1b834f5d746
sajeetharan <sajeefx@gmail.com>
2021-08-24 15:03:14 +0530

    ES6 changes Module updates Folder structure

--- BookStoreBackend/src/db/getBook.js
+++
@@ -1,9 +0,0 @@
-import { db } from './db';
-import { ObjectId } from "bson"
-
-export const getBook = async (bookId) => {
-    const connection = db.getConnection();
-    // Query one document that matches the particular criteria
-    const book = await connection.collection('books').findOne({"_id": ObjectId(bookId)})
-    return book;
-}
---
+++ server/src/db/db.js
@@ -0,0 +1,29 @@
+import  MongoClient  from 'mongodb';
+
+const DB_NAME = 'cosmosbookstore';
+
+const db = {
+    _dbClient: null,
+    connect: async function(url) {
+        // Set up singleton client connecting to Cosmos DB API for MongoDB
+
+        // Use connection pool size of 10 by default
+        const client = await MongoClient.connect(url, {
+            poolSize: 10,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Cosmos DB's API for MongoDB connected")
+        this._dbClient = client;
+    },
+    getConnection: function() {
+        if (!this._dbClient) {
+            console.log('You need to call .connect() first!');
+            process.exit(1);
+        }
+
+        return this._dbClient.db(DB_NAME);
+    }
+}
+export default db
+export {db}
